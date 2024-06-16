67944ebdbb3113f3fc3acf7d9b96f1ce4944b6d8
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-06-16 23:12:38 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/financialInstrumentsSubscriptionsManager/FinancialInstrumentsSubscriptionsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.financialInstrumentsSubscriptionsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInstrumentsSubscriptionsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialInstrumentsSubscriptionsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piotrgrochowiecki/financialInstrumentsSubscriptionsManager/FinancialInstrumentsSubscriptionsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.financialInstrumentsSubscriptionsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInstrumentsSubscriptionsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
