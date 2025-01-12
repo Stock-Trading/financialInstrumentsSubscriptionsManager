2d1d1f3cd5e5f116dae781dd6aadfa08add7b380
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2025-01-12 21:47:48 +0100

    enabled scheduling

--- src/main/java/com/piotrgrochowiecki/financialInstrumentsSubscriptionsManager/FinancialInstrumentsSubscriptionsManagerApplication.java
+++ src/main/java/com/piotrgrochowiecki/financialInstrumentsSubscriptionsManager/FinancialInstrumentsSubscriptionsManagerApplication.java
@@ -1,13 +1,14 @@
 package com.piotrgrochowiecki.financialInstrumentsSubscriptionsManager;
 
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableScheduling
 @Log4j2
 public class FinancialInstrumentsSubscriptionsManagerApplication {
 
